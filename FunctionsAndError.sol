24fb2fbc861a5e581575c92e29cd18554d349f09
Diksha Joshi <148073630+DikshaJoshi5639@users.noreply.github.com>
2024-06-13 18:25:40 +0530

    FunctionsAndError.sol

--- FunctionsAndError.sol
+++ FunctionsAndError.sol
@@ -1,38 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SupplyChain {
-    address public seller;
-    uint256 public itemPrice;
-    uint256 public totalItemsInStock;
+contract Mytoken {
+    uint public CountOrder;
+    mapping(uint => address) public orderOwners;
+    mapping(uint => uint) public orderPizza;
+    mapping(uint => bool) public isDelivered;
+    mapping(uint => bool) public isRejected;
+    
+    event OrderDelivered(uint orderNumber, address client);
+    event OrderRejected(uint orderNumber, address client);
+    
 
-    constructor(uint256 _itemPrice, uint256 _totalItemsInStock) {
-        seller = msg.sender;
-        itemPrice = _itemPrice;
-        totalItemsInStock = _totalItemsInStock;
+    // REQUIRE FUNCTION()
+    function placeOrder(uint SelectPizza) public payable returns(uint) {
+        require(msg.value >= 1, "The Required Payment not met!");
+        CountOrder++;
+        orderOwners[CountOrder] = msg.sender;
+        orderPizza[CountOrder] = SelectPizza;
+        return CountOrder;
+    }
+    
+    // ASSERT FUNCTION()
+    function delivered(uint orderNumber) public {
+    assert(orderOwners[orderNumber] == msg.sender && !isDelivered[orderNumber]);
+    
+    isDelivered[orderNumber] = true;
+    emit OrderDelivered(orderNumber, msg.sender);
     }
 
-    //REQUIRE() METHOD
-    function buyItem(uint256 quantity) external payable {
-        require(quantity <= totalItemsInStock, "Not enough items in stock");
-
-        totalItemsInStock -= quantity;
+    // REVERT FUNCTION()
+    function rejectOrder(uint orderNumber) public {
+    if (orderOwners[orderNumber] != msg.sender || isRejected[orderNumber]) {
+        revert("Order does not exist or has already been rejected");
     }
 
-    // ASSERT() METHOD
-    function restockItems(uint256 quantity) external {
-        assert(quantity > 0); 
-        
-        totalItemsInStock += quantity;
-    }
+    isRejected[orderNumber] = true;
+    emit OrderRejected(orderNumber, msg.sender);
+}
 
-    // REVERT() METHOD
-    function setItemPrice(uint256 _itemPrice) external {
-
-        if (_itemPrice == 0) {
-            revert("Item price Can Not be Zero");
-        }
-        
-        itemPrice += _itemPrice;
-    }
 }
